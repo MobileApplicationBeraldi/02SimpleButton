c695ef5c604b9049026d3050e37398ae40ca3bfc
RBeraldi <beraldi@dis.uniroma1.it>
2017-09-26 12:28:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/roberto/simplebutton/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.roberto.simplebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements Button.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button2);
+        //btn.setOnClickListener(new listener());
+        btn.setOnClickListener(this);
+    }
+
+    public void btnClicked(View view) {
+        Button btn = (Button) findViewById(R.id.button);
+                btn.setText("OK");
+        Toast.makeText(this,"btn 1 clicked....",Toast.LENGTH_SHORT).show();
+    }
+
+    class listener implements Button.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplication().getApplicationContext(),"btn 2 clicked....",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplication().getApplicationContext(),"btn clicked....",Toast.LENGTH_SHORT).show();
+
+    }
+}
